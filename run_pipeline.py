00bd244b524452e275a41865ad12e918a5686444
radustefanionete <radustefan.i@students.opit.com>
2025-03-23 19:40:12 +0100

    Update run_pipeline.py

--- run_pipeline.py
+++ run_pipeline.py
@@ -8,26 +8,27 @@ with open("config.yaml", "r") as file:
 
 # Step 1: Preprocessing Data
 print("Running Text Preprocessing...")
-subprocess.run(["python", "src/preprocess_text.py"])
+subprocess.run(["python", "src/preprocess/preprocess_text.py"])
 
 print("Running Image Preprocessing...")
-subprocess.run(["python", "src/preprocess_images.py"])
+subprocess.run(["python", "src/preprocess/preprocess_image.py"])
 
 # Step 2: Training Models
 print("Training Text Model...")
-subprocess.run(["python", "train/train_text_model.py"])
+subprocess.run(["python", "src/models/train_nlp.py"])
 
 print("Training Image Model...")
-subprocess.run(["python", "train/train_image_model.py"])
+subprocess.run(["python", "src/models/train_cv.py"])
 
 print("Training Fusion Model...")
-subprocess.run(["python", "train/train_fusion.py"])
+subprocess.run(["python", "src/models/final_model.py"])
 
 # Step 3: Evaluating Model
-    print("Evaluating Model Performance...")
-    run_script(EVALUATE_MODEL)
+print("Evaluating Model Performance...")
+subprocess.run(["python", "src/evaluation/evaluate.py"])
 
 print("Pipeline completed successfully!")
 
 if __name__ == "__main__":
+    # Running the pipeline
     main()
