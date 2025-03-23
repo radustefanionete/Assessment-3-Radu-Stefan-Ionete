3feb53e8dedfc11f08249c7332184c7421587d48
radustefanionete <radustefan.i@students.opit.com>
2025-03-23 21:13:47 +0100

    Update download_data.py

--- src/preprocessing/download_data.py
+++ src/preprocessing/download_data.py
@@ -1,22 +1,20 @@
-import zipfile
 import os
+import zipfile
 
-# Path to the dataset zip file in Google Colab
-local_zip_path = '/content/twitter_sentiment_data.zip'
-extract_path = '/content/data/raw'
+# Defining the local dataset path
+local_zip_path = r"C:\Users\stefa\OneDrive\Documents\Stefan\OPIT - MSc Data Science & AI\Workings\Applications in Data Science & AI\twitter_sentiment_data.zip"
+extract_path = "data/raw"
 
 def extract_local_data():
     if not os.path.exists(extract_path):
         os.makedirs(extract_path)
 
     try:
-        # Extracting the dataset
         with zipfile.ZipFile(local_zip_path, 'r') as zip_ref:
             zip_ref.extractall(extract_path)
         print(f"Dataset extracted successfully to {extract_path}")
     except Exception as e:
         print(f"Error extracting dataset: {e}")
 
-# Running the extraction function
 if __name__ == "__main__":
     extract_local_data()
